8a95d7889f88106ee6d27542e1557109a32b7303
Huzaifa Naseer <huzaifanaseerkhan02@gmail.com>
2022-04-17 06:02:51 +0500

    Displayed generation number on screen

--- Flappy Bird.py
+++ Flappy Bird.py
@@ -8,6 +8,7 @@ pygame.mixer.init()
 
 WIN_WIDTH = 500
 WIN_HEIGHT = 800
+GEN = 0
 
 pygame.display.set_caption("Flapp Bird") # text show on title bar
 
@@ -162,7 +163,7 @@ class Base:
         win.blit(self.IMG, (self.x1, self.y))
         win.blit(self.IMG, (self.x2, self.y))
 
-def draw_window(win, birds, pipes, base, score):
+def draw_window(win, birds, pipes, base, score, gen):
     win.blit(BG_IMG, (0, 0))
 
     for pipe in pipes:
@@ -170,6 +171,9 @@ def draw_window(win, birds, pipes, base, score):
 
     text = STAT_FONT.render(f"Score: {score}", 1, (255, 255, 255))
     win.blit(text, (WIN_WIDTH - 10 - text.get_width(), 10))
+
+    text = STAT_FONT.render(f"Gen: {gen}", 1, (255, 255, 255))
+    win.blit(text, (10, 10))
 
     base.draw(win)
 
@@ -179,6 +183,8 @@ def draw_window(win, birds, pipes, base, score):
     pygame.display.update()
 
 def main(genomes, config):
+    global GEN
+    GEN += 1
     birds = []
     nets = []
     ge = []
@@ -266,7 +272,7 @@ def main(genomes, config):
                 DIE_SOUND.play()
 
         base.move()
-        draw_window(win, birds, pipes, base, score)
+        draw_window(win, birds, pipes, base, score, GEN)
 
 
 def run(config_path):
